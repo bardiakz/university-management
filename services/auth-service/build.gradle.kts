034ccfa91ca1fdb575cbbd90654df197b38dfa6f
bardia <bardiakzm@gmail.com>
2025-11-13 20:02:08 +0100

    init: auth-service

--- services/auth-service/build.gradle.kts
+++ services/auth-service/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("java")
+    id("application")
 }
 
-group = "org.example"
+group = "io.github.bardiakz"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -13,6 +14,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+application {
+    mainClass.set("io.github.bardiakz.Main")
 }
 
 tasks.test {
