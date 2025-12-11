48795b359df77b41fa52a54f223d3406ced0cef7
bardia <bardiakzm@gmail.com>
2025-12-11 18:29:36 +0100

    fix: deprecated Jackson2JsonMessageConverter

--- services/resource-service/src/main/java/io/github/bardiakz/resource_service/config/RabbitMQConfig.java
+++ services/resource-service/src/main/java/io/github/bardiakz/resource_service/config/RabbitMQConfig.java
@@ -3,7 +3,7 @@ package io.github.bardiakz.resource_service.config;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,13 +46,14 @@ public class RabbitMQConfig {
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        // JacksonJsonMessageConverter uses Jackson 3.x (no constructor parameters needed)
+        return new JacksonJsonMessageConverter();
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
     }
 }
