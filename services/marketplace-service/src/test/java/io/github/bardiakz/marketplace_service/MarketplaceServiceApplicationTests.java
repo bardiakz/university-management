f9d235186f45def379ba44e335df952bd3038b16
bardia <bardiakzm@gmail.com>
2025-12-25 11:48:55 +0100

    init: marketplace

---
+++ services/marketplace-service/src/test/java/io/github/bardiakz/marketplace_service/MarketplaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bardiakz.marketplace_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
