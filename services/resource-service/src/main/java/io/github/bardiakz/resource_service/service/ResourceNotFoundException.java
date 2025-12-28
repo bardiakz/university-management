564b5d802549f03a376b67fdcadb7091bc0bc8c3
Milad Dadsetani <45009518+MilDads@users.noreply.github.com>
2025-12-29 01:40:26 +0330

    Delete services/resource-service directory

--- services/resource-service/src/main/java/io/github/bardiakz/resource_service/service/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package io.github.bardiakz.resource_service.service;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
